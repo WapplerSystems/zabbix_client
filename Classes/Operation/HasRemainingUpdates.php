271224c0932582466bb52f284dfec6aec627798f
Sven Wappler <info@wappler.systems>
2019-09-22 05:57:27 +0200

    New HasRemainingUpdates operation

---
+++ Classes/Operation/HasRemainingUpdates.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WapplerSystems\ZabbixClient\Operation;
+
+/**
+ * This file is part of the "zabbix_client" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Install\Service\UpgradeWizardsService;
+use WapplerSystems\ZabbixClient\OperationResult;
+
+
+/**
+ *
+ */
+class HasRemainingUpdates implements IOperation, SingletonInterface
+{
+
+    /**
+     *
+     * @param array $parameter None
+     * @return OperationResult
+     */
+    public function execute($parameter = [])
+    {
+        $upgradeWizardsService = GeneralUtility::makeInstance(UpgradeWizardsService::class);
+        $incompleteWizards = $upgradeWizardsService->getUpgradeWizardsList();
+        $incompleteWizards = array_filter(
+            $incompleteWizards,
+            function ($wizard) {
+                return $wizard['shouldRenderWizard'];
+            }
+        );
+
+        return new OperationResult(true, count($incompleteWizards) > 0);
+    }
+}
