8bc36811cb5c3947a4cc12e793ef8b4a02d18867
Sven Wappler <info@wappler.systems>
2021-05-19 01:48:06 +0200

    Application context fix for v11

--- Classes/Operation/GetApplicationContext.php
+++ Classes/Operation/GetApplicationContext.php
@@ -9,6 +9,7 @@ namespace WapplerSystems\ZabbixClient\Operation;
  * LICENSE.txt file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use WapplerSystems\ZabbixClient\OperationResult;
@@ -22,6 +23,19 @@ class GetApplicationContext implements IOperation, SingletonInterface
      */
     public function execute($parameter = [])
     {
+        if (version_compare(TYPO3_version, '11.0.0', '>=')) {
+            $applicationContext = Environment::getContext();
+            if ($applicationContext->isDevelopment()) {
+                return new OperationResult(true, 'Development');
+            }
+            if ($applicationContext->isTesting()) {
+                return new OperationResult(true, 'Testing');
+            }
+            if ($applicationContext->isProduction()) {
+                return new OperationResult(true, 'Production');
+            }
+            return new OperationResult(true, '');
+        }
         $applicationContext = GeneralUtility::getApplicationContext();
         if ($applicationContext->isDevelopment()) {
             return new OperationResult(true, 'Development');
