40dedfe13524095ace2d17bc28ade2618a70a59a
Remo Häusler <remo.haeusler@intersim.ch>
2021-07-16 11:31:52 +0200

    [BUGFIX] Respect disabled and deleted field Resolves: #24

--- Classes/Operation/HasFailedSchedulerTask.php
+++ Classes/Operation/HasFailedSchedulerTask.php
@@ -9,6 +9,7 @@ namespace WapplerSystems\ZabbixClient\Operation;
  * LICENSE.txt file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
@@ -28,14 +29,21 @@ class HasFailedSchedulerTask implements IOperation, SingletonInterface
     public function execute($parameter = [])
     {
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_scheduler_task');
-        $count = $queryBuilder
+        $queryBuilder
             ->count('uid')
             ->from('tx_scheduler_task')
             ->where(
+                $queryBuilder->expr()->eq('disable', $queryBuilder->createNamedParameter(0, Connection::PARAM_INT)),
                 $queryBuilder->expr()->neq('lastexecution_failure', $queryBuilder->createNamedParameter(''))
-                )
-            ->execute()
-            ->fetchColumn(0);
+            );
+
+        if (version_compare(TYPO3_version, '9.0.0', '>=')) {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->eq('deleted', $queryBuilder->createNamedParameter(0, Connection::PARAM_INT))
+            );
+        }
+
+        $count = $queryBuilder->execute()->fetchColumn(0);
 
         return new OperationResult(true, $count > 0);
     }
